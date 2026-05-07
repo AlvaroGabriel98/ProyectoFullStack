bec0773c0d45b877f96e68b67d6a8605b7496a40
IgnacioMontecinos <ig.montecinosdiaz@gmail.com>
2026-05-07 00:25:38 -0400

    Segui avanzando poniendo codigo en el service y repository

--- ms-stock/ms-stock/src/main/controller/StockController.java
+++ ms-stock/ms-stock/src/main/controller/StockController.java
@@ -1,3 +1,41 @@
+package com.ms_stock.controller;
+
+import example.ms_stock.model.Stock;
+import example.ms_stock.service.StockService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/stock")
 public class StockController {
+     @Autowired
+    private StockService stockService;
+
+    
+    @GetMapping("/validar/{productoId}")
+    public ResponseEntity<Boolean> validar(@PathVariable Long productoId) {
+        return ResponseEntity.ok(stockService.tieneStock(productoId));
+    }
+
+   
+    @GetMapping("/producto/{productoId}")
+    public ResponseEntity<Stock> obtenerDetalle(@PathVariable Long productoId) {
+        return ResponseEntity.ok(stockService.obtenerPorProductoId(productoId));
+    }
+
+    
+    @PutMapping("/descontar/{productoId}")
+    public ResponseEntity<String> descontar(@PathVariable Long productoId, @RequestParam Integer cantidad) {
+        stockService.descontarStock(productoId, cantidad);
+        return ResponseEntity.ok("Stock actualizado correctamente");
+    }
+
+
+    @GetMapping("/test-error/{id}")
+    public ResponseEntity<?> testError(@PathVariable Long id) {
+        return ResponseEntity.ok(stockService.obtenerPorProductoId(id));
+    }
 
 }
